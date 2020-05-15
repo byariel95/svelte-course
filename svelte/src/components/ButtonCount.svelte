3d27aff31a8f484bb0a866be4cf84f544b0f820a
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2020-05-14 22:11:32 -0600

    store in svelte

---
+++ svelte/src/components/ButtonCount.svelte
@@ -0,0 +1,23 @@
+<script>
+ import { count } from '../store/store.js';
+
+ function handlClick() {
+     count.update(n => n + 1);
+ }
+     
+</script>
+
+<style>
+button {
+    color: white;
+    background-color: #f76027;
+    border: none;
+    border-radius: 5px;
+    padding: 0.5rem;
+    text-transform: uppercase;
+    cursor: pointer;
+
+}
+</style>
+
+<button on:click={handlClick}> Click {$count === 0 ? '': $count} </button>
